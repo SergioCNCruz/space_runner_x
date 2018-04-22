9272fdb5555d98e6e46fc06e5c91a47f55102b4f
Sérgio C. N. Cruz <sergiocncruz@gmail.com>
2018-04-22 02:45:40 -0300

    First try to implement side movement in the ship

--- src/js/Ship.js
+++ src/js/Ship.js
@@ -4,24 +4,36 @@ class Ship {
         this.y = y;
         this.width = 50;
         this.height = 50;
+        this.y_axis = 0;
     }
 
     draw() {
+        stroke(179, 179, 179);
+        strokeWeight(12);
+        line(this.x - this.width*0.8, this.y - 50, this.x + 5, (this.y + this.y_axis));
+        line(this.x - this.width*0.8, this.y + 50, this.x + 5, (this.y + this.y_axis));
+
         stroke(204, 204, 204);
         strokeWeight(12);
-        line(this.x - this.width*0.75, this.y - 50, this.x, this.y);
-        line(this.x - this.width*0.75, this.y + 50, this.x, this.y);
+        line(this.x - this.width*0.8, this.y - 55, this.x + 5, (this.y + this.y_axis) + 5);
+        line(this.x - this.width*0.8, this.y + 55, this.x + 5, (this.y + this.y_axis) - 5);
+
+        noStroke();
+        fill(166, 166, 166);
+        rect(this.x - (this.width), this.y - (57 + this.y_axis), 70, 15, 5);
+        rect(this.x - (this.width), this.y + (42 + this.y_axis*-1), 70, 15, 5);
 
         noStroke();
         fill(255, 255, 255);
-        rect(this.x - (this.width), this.y - 60, 60, 15, 60);
-        rect(this.x - (this.width), this.y + 45, 60, 15, 60);
+        rect(this.x - (this.width), this.y - (62 + this.y_axis), 70, 15, 5);
+        rect(this.x - (this.width), this.y + (47 + this.y_axis*-1), 70, 15, 5);
 
         fill(255, 255, 255);
         ellipse(this.x, this.y, this.width, this.height);
 
         fill(0, 0, 0, 50);
         ellipse(this.x + (this.width*0.35), this.y, this.width*0.25, this.height*0.5);
+        this.y_axis = 0;
     }
 
     move() {
@@ -40,12 +52,14 @@ class Ship {
         if (keyIsDown(UP_ARROW)) {
             if (this.y > this.height) {
                 this.y -= 5;
+                this.y_axis = 5;
             }
         }
 
         if (keyIsDown(DOWN_ARROW)) {
             if (this.y < (height - this.height)) {
                 this.y += 5;
+                this.y_axis = -5;
             }
         }
     }
--- src/js/main.js
+++ src/js/main.js
@@ -10,7 +10,7 @@ function setup() {
     stage = createCanvas(1000, 500);
     stage.parent("stage");
     background(0, 0, 0);
-    ship = new Ship(0, height/2);
+    ship = new Ship(50, height/2);
 
     for(let a = 0; a < 8;a++) {
         asteroids.push(new Asteroid());
